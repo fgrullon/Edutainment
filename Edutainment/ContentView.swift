416f20d447cddae291d358128bc4e78bfaec4792
fgrullon <franfelix@gmail.com>
2024-08-06 18:21:39 -0400

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by Frank Grullon on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Frank Grullon on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
